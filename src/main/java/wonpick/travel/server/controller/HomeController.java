aa1bcd4778619246f83797aa463b6729cacb1ee2
LeeYeonhee-00 <dldusgml0929@gachon.ac.kr>
2024-11-06 16:23:41 +0900

    feat : Logger 추가

--- src/main/java/wonpick/travel/server/controller/HomeController.java
+++ src/main/java/wonpick/travel/server/controller/HomeController.java
@@ -1,6 +1,8 @@
 package wonpick.travel.server.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+    private static final Logger logger = LogManager.getLogger(HomeController.class);
+
     @GetMapping("/test")
     public String hello() {
+        logger.info("[travelwonpick] test api 요청");
         return "테스트입니다.";
     }
 }
-
 
