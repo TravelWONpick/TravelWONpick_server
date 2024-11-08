a508beba70a7209ac7057365446033907e4b8c39
lisiant <wkdxod321@gmail.com>
2024-11-08 15:39:30 +0900

    Feat: 특가픽 엔티티 수정 - 출발지/도착지 공항 코드 추가

--- src/main/java/wonpick/travel/server/entity/SpecialPricePick.java
+++ src/main/java/wonpick/travel/server/entity/SpecialPricePick.java
@@ -44,6 +44,12 @@ public class SpecialPricePick extends BaseEntity {
     @Column(name = "image_url", nullable = false)
     private String imageUrl;
 
+    @Column(name="departure_airport_code", nullable = false, length=20)
+    private String depAirportCode;
+
+    @Column(name="arrival_airport_code", nullable = false, length=20)
+    private String arrAirportCode;
+
     // Flight 엔티티와의 일대다 관계 설정
     @OneToMany(mappedBy = "specialPricePick", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Flight> flights;
