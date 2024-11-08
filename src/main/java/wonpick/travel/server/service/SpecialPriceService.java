7f293a1ca2f18aa5ec2ae8828e38ced88670401c
lisiant <wkdxod321@gmail.com>
2024-11-08 15:37:02 +0900

    Feat: 특가픽 SpecialPriceService 수정

--- src/main/java/wonpick/travel/server/service/SpecialPriceService.java
+++ src/main/java/wonpick/travel/server/service/SpecialPriceService.java
@@ -3,14 +3,11 @@ package wonpick.travel.server.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import wonpick.travel.server.dto.FlightDTO;
 import wonpick.travel.server.dto.GetSpecialPriceListResponse;
 import wonpick.travel.server.dto.SpecialPriceDTO;
-import wonpick.travel.server.entity.SpecialPricePick;
 import wonpick.travel.server.repository.SpecialPriceRepository;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,42 +16,44 @@ public class SpecialPriceService {
     private final SpecialPriceRepository specialPriceRepository;
 
     public GetSpecialPriceListResponse getAllSpecialPricesWithFlights() {
-        List<SpecialPricePick> specialPrices = specialPriceRepository.findAllWithFlights();
+//        List<SpecialPricePick> specialPrices = specialPriceRepository.findCurrentSpecialPrices(LocalDateTime.now());
 
-        List<SpecialPriceDTO> specialPriceDTOs = specialPrices.stream()
-                .map(specialPrice -> {
-                    List<FlightDTO> flightDTOs = specialPrice.getFlights().stream()
-                            .map(flight -> new FlightDTO(
-                                    flight.getId(),
-                                    flight.getSpecialPricePick().getId(),
-                                    flight.getAirline(),
-                                    flight.getFlightNumber(),
-                                    flight.getDeparturePlace(),
-                                    flight.getArrivalPlace(),
-                                    flight.getDepartureTime(),
-                                    flight.getArrivalTime(),
-                                    flight.getMaxSeat(),
-                                    flight.getOriginPrice(),
-                                    flight.getSpecialPrice(),
-                                    flight.getDepartureAirportCode(),
-                                    flight.getArrivalAirportCode(),
-                                    flight.getBaggage()
-                            ))
-                            .collect(Collectors.toList());
+        List<SpecialPriceDTO> specialPriceDTOs = specialPriceRepository.findAllWithMinPrice();
 
-                    return new SpecialPriceDTO(
-                            specialPrice.getId(),
-                            specialPrice.getDestination(),
-                            specialPrice.getDepartureDate(),
-                            specialPrice.getTitle(),
-                            specialPrice.getDescription(),
-                            specialPrice.getOpenTime(),
-                            specialPrice.getCloseTime(),
-                            specialPrice.getCategory(),
-                            flightDTOs
-                    );
-                })
-                .collect(Collectors.toList());
+//        List<SpecialPriceDTO> specialPriceDTOs = specialPrices.stream()
+//                .map(specialPrice -> {
+//                    List<FlightDTO> flightDTOs = specialPrice.getFlights().stream()
+//                            .map(flight -> new FlightDTO(
+//                                    flight.getId(),
+//                                    flight.getSpecialPricePick().getId(),
+//                                    flight.getAirline(),
+//                                    flight.getFlightNumber(),
+//                                    flight.getDeparturePlace(),
+//                                    flight.getArrivalPlace(),
+//                                    flight.getDepartureTime(),
+//                                    flight.getArrivalTime(),
+//                                    flight.getMaxSeat(),
+//                                    flight.getOriginPrice(),
+//                                    flight.getSpecialPrice(),
+//                                    flight.getDepartureAirportCode(),
+//                                    flight.getArrivalAirportCode(),
+//                                    flight.getBaggage()
+//                            ))
+//                            .collect(Collectors.toList());
+//
+//                    return new SpecialPriceDTO(
+//                            specialPrice.getId(),
+//                            specialPrice.getDestination(),
+//                            specialPrice.getDepartureDate(),
+//                            specialPrice.getTitle(),
+//                            specialPrice.getDescription(),
+//                            specialPrice.getOpenTime(),
+//                            specialPrice.getCloseTime(),
+//                            specialPrice.getCategory(),
+//                            flightDTOs
+//                    );
+//                })
+//                .collect(Collectors.toList());
 
         return new GetSpecialPriceListResponse(specialPriceDTOs);
     }
