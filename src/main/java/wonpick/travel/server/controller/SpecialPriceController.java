af2e434de47fc345632c99b4cf33b4e834f0f377
lisiant <wkdxod321@gmail.com>
2024-11-08 15:37:20 +0900

    Feat: 특가픽 SpecialPriceController 수정

--- src/main/java/wonpick/travel/server/controller/SpecialPriceController.java
+++ src/main/java/wonpick/travel/server/controller/SpecialPriceController.java
@@ -1,4 +1,25 @@
 package wonpick.travel.server.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wonpick.travel.server.dto.BaseResponse;
+import wonpick.travel.server.dto.GetSpecialPriceListResponse;
+import wonpick.travel.server.dto.SpecialPriceDTO;
+import wonpick.travel.server.service.SpecialPriceService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class SpecialPriceController {
+    private final SpecialPriceService specialPriceService;
+
+    @GetMapping("/special")
+    public ResponseEntity<BaseResponse<GetSpecialPriceListResponse>> getSpecialPrices() {
+        GetSpecialPriceListResponse allSpecialPricesWithFlights = specialPriceService.getAllSpecialPricesWithFlights();
+        return ResponseEntity.ok(BaseResponse.success(allSpecialPricesWithFlights));
+    }
 }
