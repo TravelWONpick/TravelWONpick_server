4de97a19105f28ba5a1558fbc2e68c5042eea874
부준혁 <127727927+BooJunhyuk@users.noreply.github.com>
2024-11-13 17:43:05 +0900

    feat : 마이페이지 탑승객 정보 CUD 기능 API

---
+++ src/main/java/wonpick/travel/server/dto/PostPassengerRequestDTO.java
@@ -0,0 +1,17 @@
+package wonpick.travel.server.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import wonpick.travel.server.entity.enums.Gender;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostPassengerRequestDTO {
+    private String firstName;
+    private String lastName;
+    private String birth;
+    private Gender gender;
+    private String phoneNumber;
+}
---
+++ src/main/java/wonpick/travel/server/dto/PostPassengerResponseDTO.java
@@ -0,0 +1,14 @@
+package wonpick.travel.server.dto;
+
+import lombok.Getter;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostPassengerResponseDTO {
+    private Long id;
+}
