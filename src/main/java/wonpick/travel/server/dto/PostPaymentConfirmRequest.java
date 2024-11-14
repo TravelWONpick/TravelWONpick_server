0ca4d8468f0bc7f29d484e31cecd4879fd3aed53
lisiant <wkdxod321@gmail.com>
2024-11-14 17:16:10 +0900

    Feat: PostPaymentConfirmRequest 수정

--- src/main/java/wonpick/travel/server/dto/PostPaymentConfirmRequest.java
+++ src/main/java/wonpick/travel/server/dto/PostPaymentConfirmRequest.java
@@ -3,12 +3,13 @@ package wonpick.travel.server.dto;
 
 import lombok.*;
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Data
+@ToString
 public class PostPaymentConfirmRequest {
     private String orderId;
     private Integer amount;
     private String paymentKey;
+    private Long depFlightId;
+    private Long arrFlightId;
+    private Long seatCount;
 }
