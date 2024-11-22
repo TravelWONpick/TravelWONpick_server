569cc2fdef5da9ca4402f7b3f4d3a394fb94e60a
gato-46 <qud406@gmail.com>
2024-11-22 14:51:21 +0900

    fix : 이벤트 컨트롤러 api 경로 수정 및 예외처리 추가

--- src/main/java/wonpick/travel/server/service/FlightService.java
+++ src/main/java/wonpick/travel/server/service/FlightService.java
@@ -2,6 +2,8 @@ package wonpick.travel.server.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import wonpick.travel.server.common.exception.BaseException;
+import wonpick.travel.server.common.exception.ErrorCode;
 import wonpick.travel.server.dto.FlightDTO;
 import wonpick.travel.server.entity.Flight;
 import wonpick.travel.server.repository.FlightRepository;
@@ -21,7 +23,7 @@ public class FlightService {
 
     public Flight findFlightById(Long flightId) {
         return flightRepository.findById(flightId)
-                .orElseThrow(() -> new RuntimeException("항공편 정보를 찾을 수 없습니다."));
+                .orElseThrow(() -> new BaseException(ErrorCode.FLIGHT_NOT_FOUND, "해당 항공편을 찾을 수 없습니다."));
     }
 
     public List<FlightDTO> searchFlights(Long spId, String depAirportCode, String arrAirportCode, String departureDate) {
@@ -52,15 +54,18 @@ public class FlightService {
     }
 
     public void adjustFlightSeatCount(Long flightId, Long seatCount) {
+        // 항공편 조회
         Flight flight = flightRepository.findById(flightId)
-                .orElseThrow(() -> new RuntimeException("해당 항공편을 찾을 수 없습니다."));
+                .orElseThrow(() -> new BaseException(ErrorCode.FLIGHT_NOT_FOUND, "해당 항공편을 찾을 수 없습니다."));
 
+        // 잔여 좌석 확인
         if (flight.getMaxSeat() >= seatCount) {
-            flight.setMaxSeat(flight.getMaxSeat() - seatCount);
-            flightRepository.save(flight);
+            flight.setMaxSeat(flight.getMaxSeat() - seatCount); // 좌석 차감
+            flightRepository.save(flight); // 업데이트 저장
         } else {
-            throw new RuntimeException("잔여 좌석이 부족합니다.");
+            throw new BaseException(ErrorCode.SEAT_COUNT_EXCEEDS, "잔여 좌석이 부족합니다.");
         }
     }
+
 
 }
